b0082bd1ace1b441b75aa8485bd8fdcef05a80f6
Mostafa Hameda <mostafaxd12@gmail.com>
2025-07-31 11:10:06 +0300

    d

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mostafa-hameda-portfolio/', 
   plugins: [tailwindcss(),
 react()], 
 })
